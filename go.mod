3ad882d9b92bc9d3383167cd4422182ad541fdfb
Travis Jeffery <tj@travisjeffery.com>
2021-05-31 04:07:14 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/travisjeffery/certmagic-sql
+
+go 1.16
+
+require (
+	github.com/caddyserver/certmagic v0.13.1
+	github.com/lib/pq v1.3.0
+	github.com/stretchr/testify v1.5.1 // indirect
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
+	golang.org/x/tools v0.0.0-20200103221440-774c71fcf114 // indirect
+)
---
+++ storage.go
@@ -0,0 +1,202 @@
+package certmagicsql
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/caddyserver/certmagic"
+)
+
+var (
+	_ certmagic.Storage = (*Storage)(nil)
+)
+
+func NewStorage(db *sql.DB, options Options) (*Storage, error) {
+	if options.QueryTimeout == 0 {
+		options.QueryTimeout = time.Second * 3
+	}
+	if options.LockTimeout == 0 {
+		options.LockTimeout = time.Minute
+	}
+	s := &Storage{
+		db:           db,
+		queryTimeout: options.QueryTimeout,
+		lockTimeout:  options.LockTimeout,
+	}
+	return s, s.ensureTableSetup()
+}
+
+type Storage struct {
+	db           *sql.DB
+	queryTimeout time.Duration
+	lockTimeout  time.Duration
+}
+
+type Options struct {
+	QueryTimeout time.Duration
+	LockTimeout  time.Duration
+}
+
+func (s *Storage) ensureTableSetup() error {
+	ctx, cancel := context.WithTimeout(context.Background(), s.queryTimeout)
+	defer cancel()
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+	_, err = tx.ExecContext(ctx, `create table if not exists certmagic_data (
+key text primary key,
+value bytea,
+modified timestamptz default current_timestamp
+)`)
+	if err != nil {
+		return err
+	}
+	_, err = tx.ExecContext(ctx, `create table if not exists certmagic_locks (
+key text primary key,
+expires timestamptz default current_timestamp
+)`)
+	if err != nil {
+		return err
+	}
+	return tx.Commit()
+}
+
+// Lock the key and implement certmagic.Storage.Lock.
+func (s *Storage) Lock(ctx context.Context, key string) error {
+	ctx, cancel := context.WithTimeout(ctx, s.queryTimeout)
+	defer cancel()
+
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	if err := s.isLocked(tx, key); err != nil {
+		return err
+	}
+
+	expires := time.Now().Add(s.lockTimeout)
+	if _, err := tx.ExecContext(ctx, `insert into certmagic_locks (key, expires) values ($1, $2) on conflict (key) do update set expires = $2`, key, expires); err != nil {
+		return fmt.Errorf("failed to lock key: %s: %w", key, err)
+	}
+
+	return tx.Commit()
+}
+
+// Unlock the key and implement certmagic.Storage.Unlock.
+func (s *Storage) Unlock(key string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), s.queryTimeout)
+	defer cancel()
+	_, err := s.db.ExecContext(ctx, `delete from certmagic_locks where key = $1`, key)
+	return err
+}
+
+type queryer interface {
+	QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row
+}
+
+// isLocked returns nil if the key is not locked.
+func (s *Storage) isLocked(queryer queryer, key string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), s.queryTimeout)
+	defer cancel()
+	row := queryer.QueryRowContext(ctx, `select exists(select 1 from certmagic_locks where key = $1 and expires > current_timestamp)`, key)
+	var locked bool
+	if err := row.Scan(&locked); err != nil {
+		return err
+	}
+	if locked {
+		return fmt.Errorf("key is locked: %s", key)
+	}
+	return nil
+}
+
+// Store puts value at key.
+func (s *Storage) Store(key string, value []byte) error {
+	ctx, cancel := context.WithTimeout(context.Background(), s.queryTimeout)
+	defer cancel()
+	_, err := s.db.ExecContext(ctx, "insert into certmagic_data (key, value) values ($1, $2) on conflict (key) do update set value = $2, modified = current_timestamp", key, value)
+	return err
+}
+
+// Load retrieves the value at key.
+func (s *Storage) Load(key string) ([]byte, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), s.queryTimeout)
+	defer cancel()
+	var value []byte
+	err := s.db.QueryRowContext(ctx, "select value from certmagic_data where key = $1", key).Scan(&value)
+	if err == sql.ErrNoRows {
+		return nil, certmagic.ErrNotExist(fmt.Errorf("key not found: %s", key))
+	}
+	return value, err
+}
+
+// Delete deletes key. An error should be
+// returned only if the key still exists
+// when the method returns.
+func (s *Storage) Delete(key string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), s.queryTimeout)
+	defer cancel()
+	_, err := s.db.ExecContext(ctx, "delete from certmagic_data where key = $1", key)
+	return err
+}
+
+// Exists returns true if the key exists
+// and there was no error checking.
+func (s *Storage) Exists(key string) bool {
+	ctx, cancel := context.WithTimeout(context.Background(), s.queryTimeout)
+	defer cancel()
+	row := s.db.QueryRowContext(ctx, "select exists(select 1 from certmagic_data where key = $1)", key)
+	var exists bool
+	err := row.Scan(&exists)
+	return err == nil && exists
+}
+
+// List returns all keys that match prefix.
+// If recursive is true, non-terminal keys
+// will be enumerated (i.e. "directories"
+// should be walked); otherwise, only keys
+// prefixed exactly by prefix will be listed.
+func (s *Storage) List(prefix string, recursive bool) ([]string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), s.queryTimeout)
+	defer cancel()
+	if recursive {
+		return nil, fmt.Errorf("recursive not supported")
+	}
+	rows, err := s.db.QueryContext(ctx, fmt.Sprintf(`select key from certmagic_data where key like '%s%%'`, prefix))
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var keys []string
+	for rows.Next() {
+		var key string
+		if err := rows.Scan(&key); err != nil {
+			return nil, err
+		}
+		keys = append(keys, key)
+	}
+	return keys, nil
+}
+
+// Stat returns information about key.
+func (s *Storage) Stat(key string) (certmagic.KeyInfo, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), s.queryTimeout)
+	defer cancel()
+	var modified time.Time
+	var size int64
+	row := s.db.QueryRowContext(ctx, "select length(value), modified from certmagic_data where key = $1", key)
+	err := row.Scan(&size, &modified)
+	if err != nil {
+		return certmagic.KeyInfo{}, err
+	}
+	return certmagic.KeyInfo{
+		Key:        key,
+		Modified:   modified,
+		Size:       size,
+		IsTerminal: true,
+	}, nil
+}
